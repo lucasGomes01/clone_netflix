746128b80e560cb5c6a696fdae8fe056e3ae0b80
lucasGomes01 <gomes.lucasj@gmail.com>
2021-12-01 23:21:00 -0300

    início react clone Netflix

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import Tmdb from "./Tmdb";
+
+
+
+export default () => {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // pegando a lista total
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+    }
+
+    loadAll();
+  }, []);
+
+  return (
+    <div>
+      Olá Mundo
+    </div>
+  );
+}
